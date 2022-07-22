30c5333be3f912a66bdffa672d8f2c58f080d2c8
Kirthi Nandi <kirthi.nandi@gmail.com>
2022-07-21 19:15:01 -0700

    commit tree a511fd7adeb4bbdce59de0785944b48c4357eaeb

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Add your code here
+
+/*
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+  
+  // method: "POST" is missing from the object below
+  const configurationObject = {
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function (error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
+    */
+
+    function submitData(name, email) {
+        return fetch("http://localhost:3000/users",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name, 
+                email})
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.body.textContent = object["id"];
+        })
+        .catch(function(error) {
+            document.body.textContent = error["message"];
+        })
+    };
